0dc0c216c016b52c9feab9930de9fdd10f6fbb06
Douglas Vieira da Silva <ieiradouglas@gmail.com>
2022-10-22 16:34:25 -0300

    Ajustando responsivo

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 
-export default function Button({to, text, className, onClick}){
+export default function Button({to, text, className, onClick, type}){
   return(
     <Link onClick={onClick} to={to} className={className+" text-sm text-center rounded-[5px] border-2 border-[#EDF2F4] border-solid hover:bg-[rgb(255,255,255,1)] hover:text-black"}>{text}</Link>
   )
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,28 +1,41 @@
 import {user} from '../../data'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 import Aside from '../../components/Aside'
 import Button from '../../components/Button'
 
 import {supabase} from '../../assets/supabase'
 
+
+
+
+
 function Input({type, name, id, className, onChange, value}){
   return(
-    <input onChange={onChange} value={value} type={type} name={name} id={id} className={`${className} limpar text-black text-base p-1 rounded-md`}/>
-  )
+    <input 
+      onChange={onChange} 
+      value={value} 
+      type={type} 
+      name={name} 
+      id={id} 
+      className={`${className} fields text-black outline-none text-base p-1 rounded-md`}
+    />
+    )
 }
 
 export default function Register(){
 
+  
   const [input, setInput] = useState({})
+    
+  const handleChange = (e) => {
 
-  const handleChange = (e) =>{
-    
-    setInput(prevState => ({...prevState, [e.target.name]:e.target.value}))
-    
+      setInput(prevState => ({...prevState, [e.target.name]:e.target.value}))
+
   }
-
-  function handleClear(){
+  
+ /*    
+  const handleClear = () =>{
     setInput({
       usuario:"",
       senha:"",
@@ -37,24 +50,27 @@ export default function Register(){
       cidade: "",
       estado: ""
     })
-  }
-
+    
+  } */
+  
   const handleRegister = async(e) =>{
-  const { data, error } = await supabase
-  .from('user')
-  .insert([
-    { 
-      nome: input.nome, 
-      sobrenome: input.sobrenome,
-      email: input.email,
-      profissao: input.profissao,
-      celular: input.telefone,
-      nascimento: input.nascimento,
-      cep: input.cep,
-      rua: input.rua,
-      cidade: input.cidade,
-      estado: input.estado,
-   }])
+    
+    const { data, error } = await supabase
+    .from('user')
+    .insert([
+      { 
+        nome: input.nome, 
+        sobrenome: input.sobrenome,
+        email: input.email,
+        profissao: input.profissao,
+        celular: input.telefone,
+        nascimento: input.nascimento,
+        cep: input.cep,
+        rua: input.rua,
+        cidade: input.cidade,
+        estado: input.estado,
+    }])
+
   }
 
   return(
@@ -65,22 +81,22 @@ export default function Register(){
             <div className="flex flex-col flex-1 gap-4 md:gap-4 h-[500px]  w-full">
 
               <label htmlFor="usuario">Usuário:</label>
-              <Input onChange={handleChange} value={input.usuario} type="text" name="usuario" id="usuario" className="mb-8"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.usuario} type="text" name="usuario" id="usuario" className="mb-8"/>
 
               <label htmlFor="nome">Nome:</label>
-              <Input onChange={handleChange} value={input.nome} type="text" name="nome" id="nome"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.nome} type="text" name="nome" id="nome"/>
 
               <label htmlFor="sobrenome">Sobrenome:</label>
-              <Input onChange={handleChange} value={input.sobrenome} type="text" name="sobrenome" id="sobrenome"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.sobrenome} type="text" name="sobrenome" id="sobrenome"/>
 
               <label htmlFor="email">E-mail:</label>
-              <Input onChange={handleChange} value={input.email} type='email' name="email" id="email"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.email} type='email' name="email" id="email"/>
 
               <label htmlFor="profissao">Profissão:</label>
-              <Input onChange={handleChange} value={input.profissao} type="text" name="profissao" id="profissao"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.profissao} type="text" name="profissao" id="profissao"/>
 
               <label htmlFor="telefone">Telefone:</label>
-              <Input onChange={handleChange} value={input.telefone} type='tel' name="telefone" id="telefone"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.telefone} type='tel' name="telefone" id="telefone"/>
 
             </div>
 
@@ -88,7 +104,7 @@ export default function Register(){
 
               <img src={user.img} className="rounded-full max-h-[120px] max-w-[120px] border-2 border-solid border-white"/>
 
-              <label htmlFor="arquivo" className="p-4 rounded-[5px] border-2 border-[#EDF2F4] border-solid hover:bg-[rgb(255,255,255,1)] hover:text-black hover:cursor-pointer">Enviar</label>
+              <label htmlFor="arquivo" className="p-4 rounded-[5px] border-2 border-[#EDF2F4] border-solid hover:bg-[rgb(255,255,255,1)] hover:text-black hover:cursor-pointer">Selecionar</label>
               <Input type='file' name="arquivo" id="arquivo" className="hidden"/>
 
             </div>
@@ -96,37 +112,37 @@ export default function Register(){
             <div className="flex flex-col flex-1 gap-4 md:gap-4 h-[500px]  w-full ">
 
               <label htmlFor="senha">Senha:</label>
-              <Input onChange={handleChange} value={input.senha} type="password" name="senha" id="senha" className="mb-8"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.senha} type="password" name="senha" id="senha" className="mb-8"/>
 
               <label htmlFor="cep">Cep:</label>
-              <Input onChange={handleChange} value={input.cep} type="text" name="cep" id="cep"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.cep} type="text" name="cep" id="cep"/>
 
               <label htmlFor="rua">Rua:</label>
-              <Input onChange={handleChange} value={input.rua} type="text" name="rua" id="rua"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.rua} type="text" name="rua" id="rua"/>
 
               <label htmlFor="cidade">Cidade:</label>
-              <Input onChange={handleChange} value={input.cidade} type="text" name="cidade" id="cidade"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.cidade} type="text" name="cidade" id="cidade"/>
 
               <label htmlFor="estado">Estado:</label>
-              <Input onChange={handleChange} value={input.estado} type="text" name="estado" id="estado"/>
+              <Input onChange={(e)=>{handleChange(e)}} value={input.estado} type="text" name="estado" id="estado"/>
 
               <label htmlFor="nascimento">Data de Nascimento:</label>
-              <Input onChange={handleChange} type="date" name="nascimento" id="nascimento"/>
+              <Input onChange={(e)=>{handleChange(e)}} type="date" name="nascimento" id="nascimento"/>
 
             </div>
           </div>
 
-        <Button 
-          className="p-3 w-48" 
-          text="Enviar" 
+        <button
+          className="p-3 w-48 text-sm text-center rounded-[5px] border-2 border-[#EDF2F4] border-solid hover:bg-[rgb(255,255,255,1)] hover:text-black" 
           type="submit" 
           onClick={
            ((e)=>{
+              console.log(input)
               e.preventDefault()
               handleRegister()
-              handleClear()
-          })}
-        />
+              /* handleClear() */
+          })}>Enviar
+        </button>
 
         </form>
       </div>
